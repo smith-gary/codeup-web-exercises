c44b5f50d63fbc331509454e89c0b8eb1e2f767b
gary_s <smithgd34@gmail.com>
2021-10-08 10:48:48 -0500

    Completed the JS with HTML lec.

---
+++ js/js-with-html-lec-script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Hello, World! It\'s me Regulus! In my own file now!');
+let returnOfAlert = alert('It\'s Friday Everybody!');
+console.log(returnOfAlert);
+
+let returnOfPrompt = prompt('Did you know it was Friday?');
+console.log(returnOfPrompt);
+
+let returnOfConfirm = confirm('Got plans?');
+console.log(returnOfConfirm);
+
+let friPlansInvite = confirm('Well even if you do or don\'t have plans, want to hang out?');
+console.log(friPlansInvite);
