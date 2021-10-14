51b56d92d2e3a49242203bb759c0b681816fa2c4
gary_s <smithgd34@gmail.com>
2021-10-14 09:10:25 -0500

    Ternary expression warm up complete.

--- js/warm-up.js
+++ js/warm-up.js
@@ -1,12 +1,15 @@
 'use strict';
 (function () {
 //console.log('Included JS file in HTML Document');
-function numSeven () {
-    return 7;
-    };
-    console.log(numSeven());
-
-if (false) {
-    console.log('Happening?');
-}
+// function numSeven () {
+//     return 7;
+//     };
+//     console.log(numSeven());
+//
+// if (false) {
+//     console.log('Happening?');
+// }
+    var hadBreakfast = Boolean(Math.round(Math.random()));
+    const hadBreakfastMessage = (hadBreakfast) ? 'I had breakfast this morning.' : 'I didn\'t have breakfast this morning.';
+    console.log(hadBreakfastMessage);
 })();
