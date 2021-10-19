00b0aabe4bb5eb696010377327d35e4029a5d088
gary_s <smithgd34@gmail.com>
2021-10-19 09:13:26 -0500

    Completed fizz buzz and added title to HTML doc.

--- js/warm-up.js
+++ js/warm-up.js
@@ -32,5 +32,23 @@
 
 */
 
-
+    function fizzBuzz () {
+        for (let i = 1; i <= 100; i++) {
+            // console.log(i);
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log('FizzBuzz');
+                continue;
+            }
+            if (i % 3 === 0) {
+                console.log('Fizz');
+                continue;
+            }
+            if (i % 5 === 0) {
+                console.log('Buzz');
+                continue;
+            }
+                console.log(i);
+        }
+    }
+    fizzBuzz();
 })();
