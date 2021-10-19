4bf11871a669f177918605abdca0c58bf304a468
gary_s <smithgd34@gmail.com>
2021-10-19 09:17:37 -0500

    Completed fizz buzz and added title to HTML file.

--- js/warm-up.js
+++ js/warm-up.js
@@ -38,17 +38,17 @@
             if (i % 3 === 0 && i % 5 === 0) {
                 console.log('FizzBuzz');
                 continue;
-            }
+            };
             if (i % 3 === 0) {
                 console.log('Fizz');
                 continue;
-            }
+            };
             if (i % 5 === 0) {
                 console.log('Buzz');
                 continue;
-            }
+            };
                 console.log(i);
-        }
-    }
+        };
+    };
     fizzBuzz();
 })();
