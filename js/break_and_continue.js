ed5977d7350ac621d464b26360fef1dd32a99b75
gary_s <smithgd34@gmail.com>
2021-10-19 09:19:13 -0500

    Addeding loop files from exercies.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,21 +1,21 @@
 'use strict';
-(function () {
-    function numberToSkip() {
-        let userAnswer = prompt('Enter an odd number between 1 and 50: ');
-        if (userAnswer % 2 === 0 || userAnswer < 1 || userAnswer > 50) {
-            userAnswer = prompt('That\'s an incorrect entry try again.');
-        }
-            for (let i = 1; i < 50; i++) {
-                if (i % 2 === 0) {
-                    continue;
-                }
-                if (userAnswer == i) {
-                    console.log(`Yikes! Skipping number: ${i}`);
-                    continue;
-                }
-                console.log(`Here is an odd number: ${i}`)
-            }
-        }
-
-    numberToSkip();
-})();
+// (function () {
+//     function numberToSkip() {
+//         let userAnswer = prompt('Enter an odd number between 1 and 50: ');
+//         if (userAnswer % 2 === 0 || userAnswer < 1 || userAnswer > 50) {
+//             userAnswer = prompt('That\'s an incorrect entry try again.');
+//         }
+//             for (let i = 1; i < 50; i++) {
+//                 if (i % 2 === 0) {
+//                     continue;
+//                 }
+//                 if (userAnswer == i) {
+//                     console.log(`Yikes! Skipping number: ${i}`);
+//                     continue;
+//                 }
+//                 console.log(`Here is an odd number: ${i}`)
+//             }
+//         }
+//
+//     numberToSkip();
+// })();
