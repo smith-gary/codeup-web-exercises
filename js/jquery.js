a6d140a3603125e4d8be835fb236b356f4e706d0
gary_s <smithgd34@gmail.com>
2021-11-19 11:40:10 -0600

    completed jquery mouse events exercise.

--- js/jquery.js
+++ js/jquery.js
@@ -4,9 +4,22 @@ $(document).ready(function() {
 // alert($('#list-items').html());
 // $('.codeup').css('border', '1px solid red')
 
-$('li').css('font-size', '20px');
-$('h1, p, li').css('background-color', 'yellow');
-alert($('h1').html());
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('background-color', 'yellow');
+// alert($('h1').html());
 
+$('h1.codeup').click(function(){
+    $(this).css('background-color', 'green');
+})
 
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px');
+})
+
+$('li').hover(function() {
+    $(this).css('color', 'red');
+},
+    function() {
+    $(this).css('color', 'black');
+    })
 });
