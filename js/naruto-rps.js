2b909daf3c63babc1182ce27ae747df5f90be265
gary_s <smithgd34@gmail.com>
2021-11-28 16:21:54 -0600

    Need to complete logic on displaying win/lose/draw.

--- js/naruto-rps.js
+++ js/naruto-rps.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     'use strict';
 
-    //NARUTO HOVER EFFECT BEFORE CLICK//
     let naruto = $('.naruto');
     let rock = $('.rock');
     let paper = $('.paper');
     let scissors = $('.scissors');
+    const winnerScores = [0,0];
+    const winner = $('.winner');
+    const scores = $('.scores');
+
+    //NARUTO HOVER EFFECT BEFORE CLICK//
     $('.rock, .paper, .scissors').hover(
         function() {
             $('.naruto').attr('src', 'img/naruto-battle.png');
@@ -17,13 +21,72 @@ $(document).ready(function() {
 
     //NEJI HOVER EFFECT BEFORE CLICK//
     let neji = $('.neji');
-    neji.hover(
+    $('.rock, .paper, .scissors').hover(
         function() {
-            this.src= 'img/neji-combat.png';
+            $('.neji').attr('src', 'img/neji-combat.png');
         },
         function() {
-            this.src= 'img/neji-standing%20.jpeg';
+            $('.neji').attr('src', 'img/neji-standing%20.png');
         }
     );
+ /// CHECK TO SEE WHO THE WINNER IS ///
+    function checkWinner(player, computer) {
+        if (player === 'Rock' && computer === 'Paper') {
+             return 'Computer';
+         } else if (player == 'Rock' && computer == 'Scissors') {
+             return 'Player';
+         } else if (player == 'Paper' && computer == 'Scissors') {
+             return 'Computer';
+         } else if (player == 'Paper' && computer == 'Rock') {
+             return 'Player';
+         } else if (player == 'Scissors' && computer == 'Rock') {
+             return 'Computer';
+         } else if (player == 'Scissors' && computer == 'Paper') {
+            return 'Player';
+        }
+    }
+ //// DISPLAYS WINNER IN WINNER DIV ///
+    function winnings(msg){
+        $('.winner').html(msg);
+    }
+    // EVENT LISTENERS ON IMAGES ///
 
+    $('.btn').click(function pickRPS(e) {
+        let playerSelect = e.target.innerText;
+        console.log(playerSelect);
+        let compSelect = Math.random();
+        let result = checkWinner(playerSelect, compSelect);
+        console.log(result);
+        if (result === 'Player'){
+            result += ' wins!';
+            //update score
+            winnerScores[0]++;
+        }
+
+        if (result === 'Computer'){
+            result += ' wins!';
+            winnerScores[1]++;
+        }
+        if (result === 'Draw'){
+            result += '. It\'s a tie!'
+        }
+
+        if (compSelect < .34){
+            compSelect = 'Rock';
+        } else if (compSelect <= .67){
+            compSelect = 'Paper';
+        } else {
+            compSelect= 'Scissors';
+        }
+        console.log(compSelect);
+
+
+
+       /// UPDATES SCORE IN SCORES SECTION ///
+        $('.scores').html(`Player: ${winnerScores[0]} <br> Computer: ${winnerScores[1]} `);
+
+        ///// PLAYER AND COMPUTER NAMES
+        winnings(`Player: ${playerSelect} <br> Computer: ${compSelect} <br> ${result}`);
+
+    });
 });
