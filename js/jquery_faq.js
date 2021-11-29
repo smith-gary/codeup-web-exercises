a7b97b31a0561f6c04b209a18dd0856c5101aeb0
gary_s <smithgd34@gmail.com>
2021-11-29 12:14:43 -0600

    Refactored to add actual faqs and spacing.

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
     // $('dd').addClass('invisible');
 
     // $('.invisible').css('visibility', 'hidden');
-
+    $('dt').wrap('<h1></h1>')
+    $('dd').wrap('<p></p>')
     $('.button').html('<btn class="toggler" type="button">Toggle Invisibility</btn>');
     $('.toggler').css('border', 'solid 3px black');
+    $('.toggler').css('font-size', '30px');
 
     $('.toggler').click(function(e) {
         e.preventDefault();
@@ -19,6 +21,6 @@ $(document).ready(function() {
             $(this).css('background-color', 'yellow');
         });
     });
-
+    $('p').css('font-size', '20px');
 
 });
